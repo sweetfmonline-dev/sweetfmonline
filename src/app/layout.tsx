6be4a2fa00844c80ceb0261a6f81d503f5533d61
MichKobby <88523722+MichKobby@users.noreply.github.com>
2026-02-21 19:33:16 -0500

    feat: add Google Analytics (G-5DMBBW20FP)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,23 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${merriweather.variable} antialiased bg-gray-50`}
       >
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        )}
         {process.env.NEXT_PUBLIC_ADSENSE_ID && (
           <Script
             async
